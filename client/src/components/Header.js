e87556d0223114e3b2adf9f3af58a6673649cefb
divyachhabra01 <divyachhabra034@gmail.com>
2021-12-24 19:00:32 +0530

    title changed

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,7 +4,7 @@ const Header=()=>{
   return(
     <>
   
-    <h1 className=" text-centre mx-5 my-2 font-weight-bold">Data Manager</h1>
+    <h1 className=" text-centre mx-5 my-2 font-weight-bold">Course Management System </h1>
 
 <h5 className="text-centre mx-5 my-2 font-weight-bold"> Manage your data at one place</h5>
 
